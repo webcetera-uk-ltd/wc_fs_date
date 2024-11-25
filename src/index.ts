7bf0f29b4da96f9dec6bb5a110a1b4755964821d
Steve <86303028+steve1401@users.noreply.github.com>
2024-11-25 16:57:23 +0000

    First commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { greetUser } from '$utils/greet';
-
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+/*
+ * Change the content of the year
+ */
+// when the DOM is ready
+document.addEventListener("DOMContentLoaded", function () {
+  // set the fs-hacks selector
+  const YEAR_SELECTOR = '[fs-hacks-element="hack14-year"]';
+  // get the the span element
+  const yearSpan = document.querySelector(YEAR_SELECTOR) as HTMLElement;
+  if (!yearSpan) return;
+  // get the current year
+  const currentYear = new Date().getFullYear();
+  // set the year span element's text to the current year
+  yearSpan.innerText = currentYear.toString();
 });
